a2c85b14f2cd42e27fd8b5480d191dd194b12089
Kevin Poveda <kevinpovedaj@gmail.com>
2025-09-13 09:52:23 -0500

    feat: add button to delete a complaint

--- src/routes/complaintsRoutes.js
+++ src/routes/complaintsRoutes.js
@@ -5,5 +5,6 @@ const complaintsController = require('../controllers/complaintsController');
 router.get('/list', complaintsController.listComplaints);
 router.post('/file', complaintsController.fileComplaint);
 router.get('/stats', complaintsController.complaintsStats);
+router.post('/delete', complaintsController.deleteComplaint);
 
 module.exports = router;
