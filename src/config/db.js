219a76a87a63d0221f9f3a54d436f7cd0065e51e
Kevin Poveda <kevinpoveda@gmail.com>
2025-10-11 22:00:24 -0500

    feat: implement Sequelize models for Complaint, Comment, and Entity with associations

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,15 @@
-const knex = require('knex')({
-    client: 'mysql2',
-    connection: {
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
-    },
-});
+const { Sequelize } = require('sequelize');
 
-module.exports = knex;
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    port: process.env.DB_PORT || 3306,
+    logging: false
+  }
+);
+
+module.exports = sequelize;
---
+++ src/models/index.js
@@ -0,0 +1,11 @@
+const Complaint = require('./complaint');
+const Entity = require('./entity');
+const Comment = require('./comment');
+
+Complaint.belongsTo(Entity, { foreignKey: 'id_public_entity' });
+Entity.hasMany(Complaint, { foreignKey: 'id_public_entity' });
+
+Complaint.hasMany(Comment, { foreignKey: 'id_complaint' });
+Comment.belongsTo(Complaint, { foreignKey: 'id_complaint' });
+
+module.exports = { Complaint, Entity, Comment };
