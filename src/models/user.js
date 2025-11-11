a3d990a014e7c440b3c4323e3591f3056379ac16
Luisen1 <luisenriquehernandezvarbuena@gmail.com>
2025-11-11 00:09:20 -0500

    feat: improve the readability of the user model by formatting the code

--- src/models/user.js
+++ src/models/user.js
@@ -5,31 +5,32 @@ const sequelize = require('../config/db');
 class User extends Model {}
 
 User.init({
-  id_user: { 
-    type: DataTypes.INTEGER, 
-    primaryKey: true, 
-    autoIncrement: true 
+  id_user: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
   },
-  username: { 
-    type: DataTypes.STRING(100), 
-    allowNull: false 
+  username: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+    unique: true
   },
-  password: { 
-    type: DataTypes.STRING(255), 
-    allowNull: false 
+  password: {
+    type: DataTypes.STRING(255),
+    allowNull: false
   },
-  session_status: { 
-    type: DataTypes.ENUM('active', 'inactive'), 
-    allowNull: false, 
-    defaultValue: 'inactive' 
+  session_status: {
+    type: DataTypes.ENUM('active', 'inactive'),
+    allowNull: false,
+    defaultValue: 'inactive'
   },
-  created_at: { 
-    type: DataTypes.DATE, 
-    defaultValue: DataTypes.NOW 
+  created_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
   },
-  updated_at: { 
-    type: DataTypes.DATE, 
-    defaultValue: DataTypes.NOW 
+  updated_at: {
+    type: DataTypes.DATE,
+    defaultValue: DataTypes.NOW
   }
 }, {
   sequelize,
